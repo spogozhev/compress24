f7ef4b593b9e2e59d8461085a74f9620ad926932
1artem11semenov1 <artemidusa2006@gmail.com>
2024-05-07 11:02:35 +0300

    merge main

---
+++ CMakeFiles/compress24.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/st117306/compress24/compress24.cpp" "CMakeFiles/compress24.dir/compress24.cpp.o" "gcc" "CMakeFiles/compress24.dir/compress24.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
